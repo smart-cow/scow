d3d91154bc83eabdec50a31278d13d0c43b49513
PREMA <PREMA@MM181561-PC.MITRE.ORG>
2013-06-07 13:57:32 -0400

    ThreadLocal

---
+++ cow-server/src/main/java/org/wiredwidgets/cow/server/manager/TaskServiceFactory.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.wiredwidgets.cow.server.manager;
+
+import org.jbpm.task.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.jbpm.task.service.local.LocalTaskService;
+
+/**
+ *
+ * @author PREMA
+ */
+@Component
+public class TaskServiceFactory {
+    
+    @Autowired
+    TaskService taskService;
+    
+    private ThreadLocal<org.jbpm.task.TaskService> localTaskService = new ThreadLocal<org.jbpm.task.TaskService>() {
+        /*
+        * initialValue() is called
+        */
+        @Override
+        protected org.jbpm.task.TaskService initialValue() {
+	System.out.println("Creating TaskService for Thread : " + Thread.currentThread().getName());
+        return new LocalTaskService(taskService);
+        }
+    };
+
+    public org.jbpm.task.TaskService getTaskService() {
+        return localTaskService.get();
+    }
+    
+}
